b07bd5f91aaa1b9af1df4b86432798364a6a6d0e
HaneolLee <dlgksdjf0054@gmail.com>
2021-07-15 21:37:35 +0900

    feat: create register page

---
+++ frontend/app/src/components/Register.vue
@@ -0,0 +1,220 @@
+<template>
+  <body>
+  <div class="form-body">
+    <div class="row">
+      <div class="form-holder">
+        <div class="form-content">
+          <div class="form-items">
+            <h3>회원가입</h3>
+            <form class="requires-validation" novalidate>
+              <div class="col-md-12">
+                <input class="form-control" type="text" name="id" placeholder="아이디" required>
+                <div class="valid-feedback">Username field is valid!</div>
+                <div class="invalid-feedback">Username field cannot be blank!</div>
+              </div>
+
+              <div class="col-md-12">
+                <input class="form-control" type="password" name="password" placeholder="비밀번호" required>
+                <div class="valid-feedback">Password field is valid!</div>
+                <div class="invalid-feedback">Password field cannot be blank!</div>
+              </div>
+
+              <div class="col-md-12">
+                <input class="form-control" type="text" name="name" placeholder="이름" required>
+                <div class="valid-feedback">Username field is valid!</div>
+                <div class="invalid-feedback">Username field cannot be blank!</div>
+              </div>
+
+              <div class="col-md-12">
+                <input class="form-control" type="email" name="email" placeholder="이메일" required>
+                <div class="valid-feedback">Email field is valid!</div>
+                <div class="invalid-feedback">Email field cannot be blank!</div>
+              </div>
+
+              <div class="col-md-12">
+                <select class="form-select mt-3" required>
+                  <option selected disabled value="">직업</option>
+                  <option value="jweb">Junior Web Developer</option>
+                  <option value="sweb">Senior Web Developer</option>
+                  <option value="pmanager">Project Manager</option>
+                </select>
+                <div class="valid-feedback">You selected a position!</div>
+                <div class="invalid-feedback">Please select a position!</div>
+              </div>
+
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                <label class="form-check-label">개인정보 수집에 동의합니다.</label>
+                <div class="invalid-feedback">Please confirm that the entered data are all correct!</div>
+              </div>
+
+
+              <div class="form-button mt-3">
+                <button id="submit" type="submit" class="btn btn-primary">확인</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
+</template>
+
+<script>
+export default {
+  name: "Register"
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700;900&display=swap');
+
+*, body {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+html, body {
+  height: 100%;
+  background-color: grey;
+  overflow: hidden;
+}
+
+
+.form-holder {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+}
+
+.form-holder .form-content {
+  position: relative;
+  text-align: center;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-align-items: center;
+  align-items: center;
+  padding: 60px;
+}
+
+.form-content .form-items {
+  border: 3px solid #fff;
+  padding: 40px;
+  display: inline-block;
+  width: 100%;
+  min-width: 540px;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  border-radius: 10px;
+  text-align: left;
+  -webkit-transition: all 0.4s ease;
+  transition: all 0.4s ease;
+}
+
+.form-content h3 {
+  color: #fff;
+  text-align: left;
+  font-size: 28px;
+  font-weight: 600;
+  margin-bottom: 5px;
+}
+
+.form-content h3.form-title {
+  margin-bottom: 30px;
+}
+
+.form-content p {
+  color: #fff;
+  text-align: left;
+  font-size: 17px;
+  font-weight: 300;
+  line-height: 20px;
+  margin-bottom: 30px;
+}
+
+
+.form-content label, .was-validated .form-check-input:invalid~.form-check-label, .was-validated .form-check-input:valid~.form-check-label{
+  color: #fff;
+}
+
+.form-content input[type=text], .form-content input[type=password], .form-content input[type=email], .form-content select {
+  width: 100%;
+  padding: 9px 20px;
+  text-align: left;
+  border: 0;
+  outline: 0;
+  border-radius: 6px;
+  background-color: #fff;
+  font-size: 15px;
+  font-weight: 300;
+  color: #8D8D8D;
+  -webkit-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+  margin-top: 16px;
+}
+
+
+.btn-primary{
+  background-color: #6C757D;
+  outline: none;
+  border: 0px;
+  box-shadow: none;
+}
+
+.btn-primary:hover, .btn-primary:focus, .btn-primary:active{
+  background-color: #495056;
+  outline: none !important;
+  border: none !important;
+  box-shadow: none;
+}
+
+.form-content textarea {
+  position: static !important;
+  width: 100%;
+  padding: 8px 20px;
+  border-radius: 6px;
+  text-align: left;
+  background-color: #fff;
+  border: 0;
+  font-size: 15px;
+  font-weight: 300;
+  color: #8D8D8D;
+  outline: none;
+  resize: none;
+  height: 120px;
+  -webkit-transition: none;
+  transition: none;
+  margin-bottom: 14px;
+}
+
+.form-content textarea:hover, .form-content textarea:focus {
+  border: 0;
+  background-color: #ebeff8;
+  color: #8D8D8D;
+}
+
+.mv-up{
+  margin-top: -9px !important;
+  margin-bottom: 8px !important;
+}
+
+.invalid-feedback{
+  color: #ff606e;
+}
+
+.valid-feedback{
+  color: #2acc80;
+}
+</style>
