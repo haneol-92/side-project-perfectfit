e7b95f205e788b00e208ffe1fbe457338c8648db
HaneolLee <dlgksdjf0054@gmail.com>
2021-07-16 16:58:15 +0900

    feat: modify Register logic2

--- backend/api/src/main/java/com/example/api/controller/AccountController.java
+++ backend/api/src/main/java/com/example/api/controller/AccountController.java
@@ -1,20 +1,34 @@
 package com.example.api.controller;
 
-import com.example.api.model.Stats;
+import com.example.api.entity.UserInfo;
 import com.example.api.model.User;
+
+import com.example.api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
-public class TestController {
+@RequiredArgsConstructor
+public class AccountController {
+
+  private final UserService userService;
 
   @PostMapping(path = "/users")
-  @CrossOrigin(origins = "http://localhost:8081")
-  public String getUsers(@RequestBody final User user) {
+  public String getUsers(@RequestBody UserInfo userinfo) {
 
-    System.out.println("Hello~~"+ user.toString());
     return "Hello World";
+  }
+
+  @PostMapping(path = "/useradd")
+  public String addUser(@RequestBody UserInfo userinfo) {
+
+    userService.insertUser(userinfo);
+
+    return "Join";
   }
 }
