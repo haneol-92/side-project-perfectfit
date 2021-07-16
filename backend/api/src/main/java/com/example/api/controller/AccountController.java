8ad788d01925016b257c7a88c6086583f5ad7497
HaneolLee <dlgksdjf0054@gmail.com>
2021-07-16 16:16:53 +0900

    feat: add Register Logic

--- backend/api/src/main/java/com/example/api/controller/AccountController.java
+++ backend/api/src/main/java/com/example/api/controller/AccountController.java
@@ -1,8 +1,10 @@
 package com.example.api.controller;
 
 import com.example.api.model.Stats;
+import com.example.api.model.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +12,9 @@ public class TestController {
 
   @PostMapping(path = "/users")
   @CrossOrigin(origins = "http://localhost:8081")
-  public String getUsers() {
-    System.out.println("Hello~~");
+  public String getUsers(@RequestBody final User user) {
+
+    System.out.println("Hello~~"+ user.toString());
     return "Hello World";
   }
 }
---
+++ backend/api/src/main/java/com/example/api/service/UserServiceRepository.java
@@ -0,0 +1,2 @@
+package com.example.api.service;public interface UserServiceRepository {
+}
