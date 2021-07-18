473f0b456b2125630df9cde5bf89ff876ffbc107
thewayhj <gmlwnd107!*>
2021-07-19 00:26:49 +0900

    Team List 1차 commit.

---
+++ frontend/app/src/components/Team/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <body>
+    <b-table hover :items="items" @click:row="handleClick"></b-table>
+  </body>
+</template>
+
+<script>
+export default {
+  name: "List.vue",
+  data() {
+    return {
+      fields: [
+          {key: 'No'},
+          {key: 'Sports'},
+          {key: 'region'},
+          {key: 'Name'},
+          {key: 'Leader'},
+          {key: 'Date'}
+      ],
+      items: [
+        { No: 1, Sports: 'Soccer', region: '서울', Name: 'FC 맨체스터', Leader: '김희중', Date: '2021.06.30'},
+        { No: 2, Sports: 'Basketball', region: '경기도', Name: '광명농구단', Leader: '김희중', Date: '2021.07.20'}
+      ]
+    }
+  },
+  methods: {
+    handleClick(value) {
+      this.viewDetails(value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
